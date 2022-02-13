57ea6f48ff3685b0c95e459243258edf1a258354
Carlos Gaviria <cgaviria@logyca.com>
2022-02-13 11:18:12 -0500

    cambios en la conexion

--- database.py
+++ database.py
@@ -2,5 +2,5 @@
 import motor.motor_asyncio
 from pymongo import MongoClient
 
-client_asincrono = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://carlos:Micro2020@cluster0.rxxyl.mongodb.net/prueba?retryWrites=true&w=majority")
-client_sincrono = MongoClient("mongodb+srv://carlos:Micro2020@cluster0.rxxyl.mongodb.net/prueba?retryWrites=true&w=majority")
+client_asincrono = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://carlos:Micro2020@cluster0.dfrfs.mongodb.net/test")
+client_sincrono = MongoClient("mongodb+srv://carlos:Micro2020@cluster0.dfrfs.mongodb.net/test")
